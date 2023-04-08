5b2a5cfa6a2512935cc0a5fe23cef29660cfc7a0
Serg1300 <120924062+Serg1300@users.noreply.github.com>
2023-04-09 00:02:10 +0300

    Add files via upload

---
+++ CollectionsTask1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsTask1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до текстового файла");
+            
+            string path = Console.ReadLine();
+            
+            string text = File.ReadAllText(path);            
+
+            char[] delimiters = new char[] { ' ', '\r', '\n', '\t' };
+
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            
+            var book2 = new LinkedList<string>(words);
+
+            var watchTwo = Stopwatch.StartNew();
+
+            book2.AddAfter(book2.First, "Вставка");
+
+            Console.WriteLine($"Вставка в  книгу: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+        }
+    }
+}
---
+++ CollectionsTask1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до текстового файла");
+           
+            string path = Console.ReadLine();
+            
+            string text = File.ReadAllText(path);
+
+            char[] delimiters = new char[] { ' ', '\r', '\n', '\t' };
+
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            var book1 = new List<string>(words);
+
+            var watchTwo = Stopwatch.StartNew();
+
+            book1.Insert(1, "Вставка");
+
+            Console.WriteLine($"Вставка в  книгу: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+        }
+    }
+}
